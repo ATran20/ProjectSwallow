625f5532a3552498aae723b1def3bd358c21f894
Q <49326491+qngai@users.noreply.github.com>
2020-08-26 10:41:48 +1000

    Update Qversion1_0.ino
    Able to transmit accelerometer and microphone data in utf-8 format, use python to convert to float

--- Arduino_Code/Qversion1_0.ino
+++ Arduino_Code/Qversion1_0.ino
@@ -1,67 +1,32 @@
-unsigned long timer = 0;
-long loopTime = 5000;   // microseconds
-String m_recvString = "";
-double m_scaleFactor = 2;
+const int mic = A0; //microphone
+const int xpin = A1; // x-axis of the accelerometer
+const int ypin = A2; // y-axis
+const int zpin = A3; // z-axis
 
-void setup() {
+
+void setup(){
   Serial.begin(9600);
-  timer = micros();
 }
+ 
+void loop()
+{
+  int x = analogRead(xpin); //- 331.5)/65*9.8)-28.12; //read from xpin
+  delay(1); //
+  int y = analogRead(ypin); //)- 329.5)/68.5*9.8)-26.68; //read from ypin
+  delay(1); 
+  int z = analogRead(zpin); //- 340)/68*9.8)-39.20; //read from zpin
+  delay(1);
+  int m = analogRead(mic); // read from microphone
+  float zero_G = 512.0; //ADC is 0~1023 the zero g output equal to Vs/2
+  float scale = 102.3; //ADXL335330 Sensitivity is 330mv/g
 
-void loop() {
-  timeSync(loopTime);
+
   
-  int val = (analogRead(0) - 512) *m_scaleFactor ;
-  sendToPC(&val);
-  getSerialData();
-}
-
-void timeSync(unsigned long deltaT)
-{
-  unsigned long currTime = micros();
-  long timeToDelay = deltaT - (currTime - timer);
-  if (timeToDelay > 5000)
-  {
-    delay(timeToDelay / 1000);
-    delayMicroseconds(timeToDelay % 1000);
-  }
-  else if (timeToDelay > 0)
-  {
-    delayMicroseconds(timeToDelay);
-  }
-  else
-  {
-      // timeToDelay is negative so we start immediately
-  }
-  timer = currTime + timeToDelay;
-}
-
-void sendToPC(int* data)
-{
-  byte* byteData = (byte*)(data);
-  Serial.write(byteData, 2);
-}
-
-void sendToPC(double* data)
-{
-  byte* byteData = (byte*)(data);
-  Serial.write(byteData, 4);
-}
-
-void getSerialData()
-{
-  while (Serial.available())
-  {
-    char input = Serial.read();
-    m_recvString += input;
-    if (input == '%')   // this is the end of message marker so that the program knows when to update the g_scaleFactor variable 
-    {
-      int index = m_recvString.indexOf('%');
-      String tmp = m_recvString.substring(0, index);  // remove the '%' sign
-      m_scaleFactor = tmp.toFloat();
-      m_recvString = "";
-      break;
-    }
-  }
-  m_recvString = "";  // probably redundant but just for safety
+  Serial.println((((float)x - 331.5)/65*9.8)-28.12); //print x value on serial monitor
+  Serial.println((((float)y - 329.5)/68.5*9.8)-26.68); //print y value on serial monitor
+  Serial.println((((float)z - 340.0)/68*9.8)-39.20); //print z value on serial monitor
+  Serial.println((float)m); // print microphone values on serial monitor
+  
+  
+  delay(600); //wait for 1 second 
 }
