300934d4cb9081b4874b1eb97f87d78ae905102a
Q <49326491+qngai@users.noreply.github.com>
2020-11-07 17:09:59 +1100

    Add files via upload

---
+++ Python_Code/Version2_5.py
@@ -0,0 +1,38 @@
+import serial
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.animation as FuncAnimation
+import keyboard
+
+def main():
+    port = 'COM3'
+    baudrate = 9600
+    s = serial.Serial(port, baudrate)
+    data = []
+    values = []
+    filepath = (r"C:\Users\ngaij\Desktop\Project Swallow Software\Python\Accelerometer\test.csv")
+    matrix = []
+    sensors = 6  # number of sensors
+    while True:
+        x = 0
+        array = []
+        matrix = []
+
+        if keyboard.is_pressed("a"):
+            for x in range(sensors):
+                read = s.readline().decode()
+                single = read.rstrip()
+                value = float(single)
+                array.append(value)
+                x += 1
+            if x == sensors:
+                matrix.append(array)
+                array= []
+        df = pd.DataFrame(matrix, columns=['Microphone1', 'X_Axis', 'Y_Axis', 'Z_Axis', 'Microphone2', 'Button'])
+        df.to_csv(filepath, header=True, mode="a")
+
+
+
+if __name__ == '__main__':
+    main()
